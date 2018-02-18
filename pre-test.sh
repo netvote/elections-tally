766675b205cdd4958f0501ef8e84ad3c82b3aa5e
Steven Landers <steven.landers@gmail.com>
2018-02-18 13:21:16 -0500

    using https git

--- pre-test.sh
+++ pre-test.sh
@@ -1,5 +1,5 @@
 rm -rf elections-solidity
-git clone git@github.com:netvote/elections-solidity.git
+git clone https://github.com/netvote/elections-solidity.git
 mkdir elections-solidity/test/tally
 cp test/*.js elections-solidity/test/tally
 cd elections-solidity
